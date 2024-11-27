fcb1e821fab959813be5104627edeccb08843444
Faustino Alexandre <faustinoalfredoa@outlook.com>
2024-11-27 10:03:27 +0100

    POO com modificador de acesso

--- src/poo/classe_1.ts
+++ src/poo/classe_1.ts
@@ -1,7 +1,20 @@
 class Curso {
+    private _id: number | undefined
     nome: string | undefined
+
+    get id(){
+        return this._id
+    }
+
+    set id(id: number | undefined){
+        if((id ?? 0 ) > 0){
+       this._id= id
+    }
+    }
 }
 
 const c1 = new Curso()
 c1.nome = 'Iniciando com TypeScript'
-console.log(c1);
+c1.id = 123
+console.log(c1.id);
+console.log(c1.nome);
